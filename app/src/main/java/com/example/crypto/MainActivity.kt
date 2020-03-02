f16c71dd9181ebdff6f7a7d7a3da36403bc4f785
Arunish Singh <singh.arunish1@gmail.com>
2020-03-02 22:54:26 +0530

    Basic Layout returns

--- app/src/main/java/com/example/crypto/MainActivity.kt
+++ app/src/main/java/com/example/crypto/MainActivity.kt
@@ -5,50 +5,27 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.SeekBar
 
-class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+class MainActivity : AppCompatActivity() {
 
-    var length : Int = 0
-    var key : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val encrypter : String = inputRuleText.text.toString()
-        length = encrypter.length
-        val message : String = inputText.text.toString()
-        var message1 : String = ""
-
-        for(i in message.indices){
-
-            var value = encrypter.indexOf(message[i])
-
-            if(value != -1){
-                message1 = message1.plus(encrypter[(value+key)%(encrypter.length)])
-            }
-            else{
-                message1 = message1.plus(message[i])
+        slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                inputRuleNumber.text = "$i"
             }
 
-        }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
 
-        OutputText.text = message1
-        slider.setOnSeekBarChangeListener(this)
+            }
 
-    }
-
-    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-
-        key = (length) * (progress/100)
-        inputRuleNumber.text = progress.toString()
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+        })
 
     }
-
-    override fun onStartTrackingTouch(seekBar: SeekBar?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onStopTrackingTouch(seekBar: SeekBar?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 }
