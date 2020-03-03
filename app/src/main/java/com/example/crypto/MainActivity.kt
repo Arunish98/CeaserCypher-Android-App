288a181ccdb5fa84bb65005d38f025f304cdf9f8
Arunish Singh <singh.arunish1@gmail.com>
2020-03-03 23:56:26 +0530

    step size is binded with seekbar

--- app/src/main/java/com/example/crypto/MainActivity.kt
+++ app/src/main/java/com/example/crypto/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.crypto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.SeekBar
 
@@ -11,10 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var ceaser = ceaserText.text.toString()
+        var length = ceaser.length
+
+        ceaserText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+                length = p0.length
+            }
+        })
+
+
         slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 // Display the current progress of SeekBar
-                inputRuleNumber.text = "$i"
+               // val step = (i/100)*length
+                inputRuleNumber.text = "${length*i/100}"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
